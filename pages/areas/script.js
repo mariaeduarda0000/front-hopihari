3938ae271c7d2ca7d562c6cbb56b3fae06945195
mariaeduarda0000 <mesoliveira.1977@gmail.com>
2025-06-03 17:08:55 -0300

    a

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -1,14 +1,32 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const response = await fetch(`http://localhost:3000/brinquedos/${area}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'aplication/json',
-            authorization: "Bearer" + token
-        },
-    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const area =  urlParams.get('area');
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+    const response = await fetch(`http://localhost:3001/brinquedos/area/${area}`, { 
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + token
+            },
+        });
 
     const data = await response.json();
     console.log(data);
 
+    for (var brinquedos of data.resultados){
+        console.log(brinquedos);
+
+        const grid = document.querySelector(".rides-grid");
+        grid.innerHTML = grid.innerHTML + 
+        ` <div class="ride-card">
+                <div class="ride-image" style="background-image: url('../../images/bugabalum.jpg')"></div>
+                <div class="ride-info">
+                    <h3 class="ride-name">${brinquedos.name}</h3>
+                    <div class="ride-time">${brinquedos.waiting_time} Minutos2</div>
+                    <span class="ride-status status-busy">${brinquedos.status}</span>
+                </div>
+            </div>`
+    }
 });
+
 
--- pages/perfil/script.js
+++ pages/perfil/script.js
@@ -1,26 +1,25 @@
 // Simula carregamento de dados do usuário
 const user = {
-    first_name: 'Millie',
-    last_name: 'Bobby Brown',
-    email: 'millie@example.com',
-    birth_date: '2004-02-19',
-    phone: '(11) 99999-9999'
-  };
-  
-  window.onload = () => {
-    for (const key in user) {
-      const span = document.getElementById(key);
-      if (span) span.textContent = user[key];
-    }
-  };
-  
-  const inputFoto = document.getElementById('inputFoto');
-  const fotoPerfil = document.getElementById('fotoPerfil');
-  
-  inputFoto.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      fotoPerfil.src = URL.createObjectURL(file);
-    }
-  });
-  
+  first_name: 'Millie',
+  last_name: 'Bobby Brown',
+  email: 'millie@example.com',
+  birth_date: '2004-02-19',
+  phone: '(11) 99999-9999'
+};
+
+window.onload = () => {
+  for (const key in user) {
+    const span = document.getElementById(key);
+    if (span) span.textContent = user[key];
+  }
+};
+
+const inputFoto = document.getElementById('inputFoto');
+const fotoPerfil = document.getElementById('fotoPerfil');
+
+inputFoto.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    fotoPerfil.src = URL.createObjectURL(file);
+  }
+});
